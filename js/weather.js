fa512ec1d0eed613f21aaf402d81bdfa9ee16229
khumet817 <adin.8.nell@gmail.com>
2020-02-12 11:19:32 +0200

    js code added

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+const api_url= 'http://api.openweathermap.org/data/2.5/weather?lat=-33.9258&lon=18.4232&appid=8aafc8ce235d0b557c0e033195fe71f6&units=metric';
+
+async function getData() {
+    const response = await fetch (api_url);
+    const data = await response.json();
+    const { name, weather, main, wind } = data;
+    
+    document.getElementById('city').textContent = name;
+    document.getElementById('desc').textContent = weather.description;
+    document.getElementById('hum').textContent = main.humidity;
+    document.getElementById('pressure').textContent = main.pressure;
+    document.getElementById('wind').textContent = wind.speed;
+    console.log(name);
+    console.log (weather);
+    console.log(main);
+    console.log(wind);
+}
+
+getData();
